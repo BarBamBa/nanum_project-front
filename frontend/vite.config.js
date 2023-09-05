4a6b9d8795da75520e76c6175fb07862ad680c9b
BaBamBa <127111182+BarBamBa@users.noreply.github.com>
2023-09-05 16:08:20 +0900

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -16,8 +16,8 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:9090/api",
-        // target: "http://roalwh.iptime.org:19090/api",
+        // target: "http://localhost:9090/api",
+        target: "http://roalwh.iptime.org:19090/api",
         // target: host,
         changeOrigin: true,
         secure: false,
